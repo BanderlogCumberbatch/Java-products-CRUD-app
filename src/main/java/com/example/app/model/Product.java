2afba398657b02d7860d5e7aa9e9379917cc17ff
BanderlogCumberbatch <kutlub.artur@gmail.com>
2024-07-13 01:58:40 +0400

    GET

--- src/main/java/com/example/app/model/Product.java
+++ src/main/java/com/example/app/model/Product.java
@@ -65,28 +65,61 @@ public class Product {
         this.id = id;
     }
 
+    public String getArticle() {
+        return articleNumber;
+    }
+
     public void setArticle(String articleNumber) {
         this.articleNumber = articleNumber;
+    }
+
+    public String getName() {
+        return name;
     }
             
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
     }
 
     public void setCategory(String category) {
         this.category = category;
     }
 
+    public Double getPrice() {
+        return price;
+    }
+
     public void setPrice(Double price) {
         this.price = price;
     }
 
+    public Integer getQuantity() {
+        return quantity;
+    }
+
     public void setQuantity(Integer quantity) {
         this.quantity = quantity;
+    }
+
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getLastModified() {
+        return lastModified;
     }
 
 }
