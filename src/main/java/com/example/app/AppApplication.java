f7b258c0f5bcac59d9d08f8f116fd332deaa54b1
BanderlogCumberbatch <kutlub.artur@gmail.com>
2024-07-18 10:08:33 +0400

    Local start config

--- src/main/java/com/example/app/AppApplication.java
+++ src/main/java/com/example/app/AppApplication.java
@@ -1,49 +1,13 @@
 package com.example.app;
 
-import com.example.app.model.Product;
-import com.example.app.service.ProductService;
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-@SuppressWarnings("unused")
 @SpringBootApplication
 public class AppApplication {
-	@Autowired
-	private final ProductService productService;
-	public AppApplication(ProductService productService) {
-		this.productService = productService;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
-/*
-	@Bean
-	public CommandLineRunner demo() {
-		return args -> {
-			// Тесты:
-			Product product = new Product();
-			product.setArticle("AN011");
-			product.setName("Product 1");
-			product.setDescription("Description 1");
-			product.setCategory("Category 1");
-			product.setPrice(10.);
-			product.setQuantity(5);
-			productService.createProduct(product);
-
-			Product savedProduct = productService.getProductById(product.getId());
-			System.out.println("Saved Product: " + savedProduct);
-
-		};
-		
-	}
-*/
 
 }
