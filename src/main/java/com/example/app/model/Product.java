d782e8d3b9da5561ae0bea728587239010a103b9
BanderlogCumberbatch <kutlub.artur@gmail.com>
2024-07-18 08:00:46 +0400

    Postman

--- src/main/java/com/example/app/model/Product.java
+++ src/main/java/com/example/app/model/Product.java
@@ -14,12 +14,16 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "products")
 public class Product {
 
